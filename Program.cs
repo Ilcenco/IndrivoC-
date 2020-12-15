ca370a57c20769c2aa6e4490304551ae81273cf9
Ilcenco <70934859+Ilcenco@users.noreply.github.com>
2020-12-15 22:40:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IndrivoDataBase.ConsoleMenu;
+
+namespace IndrivoDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new TournamentDBContext())
+            {
+                //Console.BackgroundColor = ConsoleColor.DarkGray;
+                //Console.ForegroundColor = ConsoleColor.Black;
+                var Menu = new StartMenu();
+                Menu.Menu();
+
+            }
+        }
+
+    }
+}
